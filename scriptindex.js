4e8c7dea63dbe4e7ee3f3a4ccda7adff28e3b30d
KaoriYes <118189343+KaoriYes@users.noreply.github.com>
2022-12-14 14:22:22 +0100

    Add files via upload

---
+++ scriptindex.js
@@ -0,0 +1,30 @@
+var button = document.querySelector(".klik");
+var buttondone = document.querySelector(".done");
+var sections = document.querySelectorAll(".animatie");
+var editbar = document.querySelector(".editbar");
+var removebutton = document.querySelectorAll(".remove");
+var accountsection = document.querySelector(".accountsection");
+
+
+
+function animatie(){
+  document.body.classList.add("wiggle");
+  editbar.classList.add("dropdown");
+  button.classList.add("opacity");
+}
+
+function animatieuit(){
+    document.body.classList.remove("wiggle");
+    document.body.classList.remove("removed");
+    editbar.classList.remove("dropdown")
+    button.classList.remove("opacity");
+}
+
+function accountsectionclick(){
+  location.href = "features.html?selection=0";
+  console.log("testaf");
+}
+
+button.addEventListener("click", animatie);
+buttondone.addEventListener("click", animatieuit);
+accountsection.addEventListener("click", accountsectionclick);
